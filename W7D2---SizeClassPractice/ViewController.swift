4f539b0c85a71dc99ec6a7abcb44d5b67b4a7977
guiworm <dylan.mccrindle@gmail.com>
2016-12-13 12:37:53 -0500

    Initial Commit

---
+++ W7D2---SizeClassPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W7D2---SizeClassPractice
+//
+//  Created by Dylan McCrindle on 2016-12-13.
+//  Copyright © 2016 Dylan-McCrindle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
